c8db105dcfcda3a94f16e9800e0c959759e8660f
xAmAx12 <xAmAx12@users.noreply.github.com>
2017-12-19 14:47:09 +0100

    Add files via upload

--- plugin.video.xAmAx-Mod/resources/Menu.py
+++ plugin.video.xAmAx-Mod/resources/Menu.py
@@ -396,46 +396,28 @@ class menu():
             return "Pas de mise à jour disponible!"
         
     def MajAuto(self): #,NomMaj,Ext,resources=""
-        """self.MajAuto("xAmAx",".db","resources/")
-        self.MajAuto("settings",".xml","resources/")
-        self.MajAuto("vStreamOpt",".py","resources/")
-        self.MajAuto("LSPOpt",".py","resources/")
-        ret = self.MajAuto("default",".py")
-        if self.adn.getSetting(id="stban")=="true":
-            self.MajAuto("Samba",".py","resources/")
-        
-        if ret == True:
-            executebuiltin('XBMC.Container.Update')
-            sleep(0.2)
-            executebuiltin('XBMC.Container.Refresh')
-            sleep(0.2)"""
-
         for NomMaj,Ext,resources in self.Maj:
             if resources=="":
                 AdresseFich = os.path.join(self.AdressePlugin, NomMaj+Ext)
             else:
                 AdresseFich = os.path.join(self.AdressePlugin, "resources", NomMaj+Ext)
-            try:
-                """AdresseVersion = self.UrlRepo+self.nomPlugin+"/"+resources+NomMaj
-                VRech = urllib.urlopen(AdresseVersion).read()
-                VLspopt = self.adn.getSetting(id=NomMaj)
-                print "Version "+NomMaj+": "+VLspopt+" Version sur internet: "+VRech"""
-                ret = self.RechMajAuto(NomMaj,resources)
-                if ret != "":
-                    DL = cDL().TelechargPage(url=self.UrlRepo+self.nomPlugin+"/"+resources+NomMaj+Ext)
-                    if not DL.startswith("Erreur"):
-                        fichier = open(AdresseFich, "w")
-                        fichier.write(DL)
-                        fichier.close()
-                        self.adn.setSetting(id=NomMaj, value=ret)
-                        print "Mise a jour de "+NomMaj+" OK"
-            except:
-                print "Erreur mise a jour: "+str(sys.exc_info()[0])
-                return "Erreur mise a jour: "+str(sys.exc_info()[0])
+            #try:
+            ret = self.RechMajAuto(NomMaj,resources)
+            if ret != "":
+                DL = cDL().TelechargPage(url=self.UrlRepo+self.nomPlugin+"/"+resources+NomMaj+Ext)
+                if not DL.startswith("Erreur"):
+                    fichier = open(AdresseFich, "w")
+                    fichier.write(DL)
+                    fichier.close()
+                    self.adn.setSetting(id=NomMaj, value=ret)
+                    print "Mise a jour de "+NomMaj+" OK"
+            #except:
+            #    print "Erreur mise a jour: "+str(sys.exc_info()[0])
+            #    return "Erreur mise a jour: "+str(sys.exc_info()[0])
         self.adn.setSetting(id="MajV", value=self.vertionMaj)
         return "OK"
     
-    def RechMajAuto(self,NomMaj,resources):
+    def RechMajAuto(self,NomMaj,resources=""):
         try:
             AdresseVersion = self.UrlRepo+self.nomPlugin+"/"+resources+NomMaj
             VRech = cDL().TelechargPage(AdresseVersion)
@@ -786,6 +768,9 @@ class menu():
                     if Retour != "OK":
                         dialog = xbmcgui.Dialog()
                         dialog.ok("Mise à jour automatique", Retour, "")
+                    else:
+                        executebuiltin('XBMC.Container.Update')
+                        executebuiltin('XBMC.Container.Refresh')
                     
             
 
